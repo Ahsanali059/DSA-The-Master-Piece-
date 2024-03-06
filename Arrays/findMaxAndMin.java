8a03a3be35a4bcce84725d0fbae827830cd46e86
Ahsan ali <ahsandev.org@gmail.com>
2024-03-06 07:44:08 +0500

    added arrys questions

---
+++ Arrays/findMaxAndMin.java
@@ -0,0 +1,40 @@
+package Arrays;
+
+public class findMaxAndMin 
+{
+    public static int findMax(int arr[])
+    {
+        int max = arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            {
+                max = arr[i];
+            }
+            
+        }
+        return max;
+        
+    }
+
+    public static int findMin(int arr[])
+    {
+        int min = arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]<min)
+            {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7};
+        
+        System.out.println(findMax(arr));
+        System.out.println(findMin(arr));
+    }
+    
+}
---
+++ Arrays/reverseArray.java
@@ -0,0 +1,46 @@
+package Arrays;
+
+public class reverseArray 
+{
+    public static void reverseArray(int arr[])
+    {
+        int leftPinter = 0;
+        int rightPointer = arr.length - 1;
+
+        while(leftPinter < rightPointer)
+        {
+            System.out.println(leftPinter);
+            
+
+
+            int temp = arr[leftPinter];
+            arr[leftPinter] = arr[rightPointer];
+            arr[rightPointer] = temp;
+            
+            leftPinter++;
+            rightPointer--;
+
+        }
+    }
+
+    public static void print(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) 
+    {
+
+        int arr[] = {10,4,1,7,8,9};
+        reverseArray(arr);
+        print(arr);
+
+
+        
+    }
+    
+}
