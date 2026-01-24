5ac6706b8444c59c98e1568b6a3a44b7fcf7b3e3
Ahsanali679 <a.ali@pioneerlogics.com>
2026-01-24 14:26:44 +0500

    added enhanced string plandrom checkc

--- strings/isValidPlandrom.java
+++ strings/isValidPlandrom.java
@@ -1,36 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // Convert string to char array
-        char arr[] = s.toCharArray();
+        int left = 0, right = s.length() - 1;
 
-        int left = 0;
-        int right = arr.length - 1;
-
-        // Reverse the array in place
         while (left < right) {
-            char temp = arr[left]; 
-            arr[left] = arr[right];
-            arr[right] = temp;
+            // Skip non-alphanumeric characters
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+
+            // Compare ignoring case
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
             left++;
             right--;
         }
 
-        // Convert reversed char array back to string
-        String revString = new String(arr);
-
-        // Compare strings
-        if (revString.equals(s)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // Optional main to test
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-
-        System.out.println(sol.isPalindrome("racecar")); // true
-        System.out.println(sol.isPalindrome("hello"));   // false
+        return true;
     }
 }
