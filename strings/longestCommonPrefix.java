700a10c9f2408d0aeb3b8c8ea3aa1f11ff67b394
Ahsanali679 <a.ali@pioneerlogics.com>
2026-01-24 13:49:23 +0500

    added common prefix problem

---
+++ strings/longestCommonPrefix.java
@@ -0,0 +1,29 @@
+// In Java, charAt() returns character of any valid index position
+// substring() returns only the part of string from startIndex to endIndex that matched so far
+class Solution {
+    public String longestCommonPrefix(String[] strs) 
+    {
+        //edge cases
+        if(strs.length ==0 || strs == null)
+        {
+            return "";
+        }
+
+        String refrence = strs[0];
+
+        for(int i=0;i<=strs.length-1;i++)
+        {
+            int index = 0;
+            while(index<refrence.length() && index<strs[i].length() 
+             && refrence.charAt(index) == strs[i].charAt(index))
+             {
+                index++;
+             }
+            
+            // shrink string
+            refrence = refrence.substring(0,index);
+        }
+
+        return refrence;
+    }
+}
