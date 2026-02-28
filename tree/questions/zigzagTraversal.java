a6c3f37ba9a8d020968df578637ec0baca34c7af
Ahsanali679 <a.ali@pioneerlogics.com>
2026-02-28 23:09:42 +0500

    added zigzag traversal

--- tree/questions/zigzagTraversal.java
+++ tree/questions/zigzagTraversal.java
@@ -2,6 +2,19 @@ package tree.questions;
 
 import java.util.*;
 
+/*
+    example:
+
+            1
+           / \
+          2   3
+         / \   \
+        4   5   6
+    zigzag level order traversal = [[1], [3, 2], [4, 5, 6]]
+
+    Time complexity: O(n) where n is the number of nodes in the tree, as we visit each node once.
+    Space complexity: O(n) in the worst case, when the tree is completely unbalanced
+*/
 public class zigzagTraversal {
 
     static class Node {
