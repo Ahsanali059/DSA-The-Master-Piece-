3c5c51c750ac922264510c241b5601b93b8a8a04
Ahsanali679 <a.ali@pioneerlogics.com>
2026-03-07 11:51:58 +0500

    added BST to dll problem

---
+++ tree/questions/BinaryTreeToDoublyLinkedList.java
@@ -0,0 +1,20 @@
+package tree.questions;
+
+/*
+    example:
+
+            1
+           / \
+          2   3
+         / \   \
+        4   5
+    binary tree to doubly linked list = 4 <-> 2 <-> 5 <-> 1 <-> 3
+    Time complexity: O(n) where n is the number of nodes in the tree, as we visit each node once.
+    Space complexity: O(n) in the worst case, when the tree is completely unbalanced
+*/
+
+public class BinaryTreeToDoublyLinkedList 
+{
+
+    
+}
---
+++ tree/questions/stringToBT.java
@@ -0,0 +1,21 @@
+package tree.questions;
+
+/*
+    example:
+
+            1
+           / \
+          2   3
+         / \   \
+        4   5   6
+
+    string to binary tree = "1(2(4)(5))(3()(6))"
+
+    Time complexity: O(n) where n is the number of nodes in the tree, as we visit each node once.
+    Space complexity: O(n) in the worst case, when the tree is completely unbalanced
+*/
+public class stringToBT 
+{
+  
+    
+}
